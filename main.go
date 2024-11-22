58a13154c8debba5f8c06212bca80978bc083816
Ethan <ethanvolk2424@gmail.com>
2024-11-22 14:55:24 -0600

    reading the file into lines

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World! (AGAIN)")
+	fmt.Println("Starting The AdventOfCode23 Script")
+	day1()
+}
+
+func day1() {
+	fmt.Println("Starting Day 1")
+
+	lines := returnlines("PuzzleInput/day1.txt")
+	fmt.Println(lines[1])
+
+}
+
+func returnlines(filepath string) []string {
+	file, _ := os.Open(filepath) //open file and ignore the errors
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines
 }
