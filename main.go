928b07f27255665a91bc3490a166a2e3696eb987
Ethan <ethanvolk2424@gmail.com>
2024-11-22 15:31:17 -0600

    working

--- main.go
+++ main.go
@@ -4,24 +4,37 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Starting The AdventOfCode23 Script")
-	day1()
+	day1a()
 }
 
-func day1() {
+func day1a() {
 	fmt.Println("Starting Day 1")
-
 	lines := returnlines("PuzzleInput/day1.txt")
-	fmt.Println(lines[1])
+	//fmt.Println(lines)
+
+	for i := 0; i < len(lines); i++ { //Loop Through Each Entry
+		j := strings.IndexAny(lines[i], "0123456789") //j is the first index of any number
+		println(lines[i])
+		println(j)
+		char := lines[i][j]
+		fmt.Printf("The Value Of The First Char is: %c", char)
+		println("next")
+
+	}
 
 }
 
 func returnlines(filepath string) []string {
-	file, _ := os.Open(filepath) //open file and ignore the errors
+	//Create VARS
 	var lines []string
+
+	//Logic
+	file, _ := os.Open(filepath) //open file and ignore the errors
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
