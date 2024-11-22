9c17a3c3f077a9f15e51cd05257e8be7591fe65a
Ethan <ethanvolk2424@gmail.com>
2024-11-22 17:17:22 -0600

    Day1a complete

--- main.go
+++ main.go
@@ -4,29 +4,39 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fmt.Println("Starting The AdventOfCode23 Script")
 	day1a()
+
+	fmt.Printf("End Of Code | Execution Duration: %s\n", time.Since(start))
 }
 
 func day1a() {
 	fmt.Println("Starting Day 1")
-	lines := returnlines("PuzzleInput/day1.txt")
-	//fmt.Println(lines)
+	lines := returnlines("PuzzleInput/day1.txt") //Read In Inputs
 
-	for i := 0; i < len(lines); i++ { //Loop Through Each Entry
-		j := strings.IndexAny(lines[i], "0123456789") //j is the first index of any number
-		println(lines[i])
-		println(j)
-		char := lines[i][j]
-		fmt.Printf("The Value Of The First Char is: %c", char)
-		println("next")
+	//Setup VARS For Today
+	answer := 0
 
+	//Loop Through Each Line
+	for i := 0; i < len(lines); i++ {
+		j := strings.IndexAny(lines[i], "0123456789")     //j is the index of the first instance of a number
+		k := strings.LastIndexAny(lines[i], "0123456789") //k is the index of the second instance of a number
+		firstchar := rune(lines[i][j])                    //Convert CHARS to RUNE
+		lastchar := rune(lines[i][k])
+		fmt.Printf("First: %c | Last: %c | String: %s\n", firstchar, lastchar, lines[i]) //Debug Line
+		linenumber := fmt.Sprint(string(firstchar), string(lastchar))                    //Using chars as a string combine them to make one string
+		num, _ := strconv.Atoi(linenumber)                                               //Convert String To Number
+		answer += num                                                                    //Add To running Total
 	}
 
+	fmt.Printf("Answer To Day 1a: %d\n", answer)
 }
 
 func returnlines(filepath string) []string {
