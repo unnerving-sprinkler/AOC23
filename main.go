f0b3c047dacfd1a72e3159316f3469c744e768a7
Ethan <ethanvolk2424@gmail.com>
2024-11-23 00:45:28 -0600

    2a working correctly

--- main.go
+++ main.go
@@ -127,22 +127,81 @@ func day1b() {
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ DAY 2A +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 func day2a() {
 	fmt.Println("\nStarting Day 2a")
-	//lines := returnlines("inputdata/day_02/day_02_test.txt") //Read In Inputs
-	lines := returnlines("inputdata/day_02/day_02_actual.txt") //Read In Inputs
+	lines := returnlines("inputdata/day_02/day_02_test.txt") //Read In Inputs
+	//lines := returnlines("inputdata/day_02/day_02_actual.txt") //Read In Inputs
+
+	//Todays VARS
+	puzzleanswer := 0
 
 	for i := 0; i < len(lines); i++ {
+		gamepossible := true
 		gameinformationstring := lines[i][(strings.Index(lines[i], ":"))+2:] //Strip The Name Of The Game Off String
-		fmt.Printf("%s\n", gameinformationstring)
+		gameinformationstring = gameinformationstring + string("; ")         //Put a Ending ; To Make Our Jobs Easier Later
+		var rounds []string
+		for {
+			rounds = append(rounds, gameinformationstring[:(strings.Index(gameinformationstring, ";"))])    //Append Each Round Info To rounds
+			gameinformationstring = gameinformationstring[(strings.Index(gameinformationstring, ";") + 2):] //Remove The Round we Just Counted From The Game
+			if len(gameinformationstring) < 4 {                                                             //Break Loop When There Are No More Rounds
+				break
+			}
+		}
+		for j := 0; j < len(rounds); j++ {
+			roundinfo := ExtractRoundInformation(rounds[j]) //For Each Game Extract Information About How Many Cubes
+			if roundinfo[0] > 12 {
+				gamepossible = false
+			}
+			if roundinfo[1] > 13 {
+				gamepossible = false
+			}
+			if roundinfo[2] > 14 {
+				gamepossible = false
+			}
+		}
+		if gamepossible {
+			puzzleanswer += ExtractGameNumber(lines[i])
+		}
 	}
-
+	fmt.Printf(">Answer To Day 2a: %d\n", puzzleanswer)
 }
 
+// Return The Game Number (Used For Day 2)
 func ExtractGameNumber(s string) int {
 	withoutprefix, _ := strings.CutPrefix(s, "Game ") //Remove the Prefix Of The Line
 	w := strings.Index(withoutprefix, ":")            //Determine Where the : Char Is
 	gamenumberstring := withoutprefix[:w]             //Strip Text String Based On That Information
 	gamenumber, _ := strconv.Atoi(gamenumberstring)   //Convert Text To int
 	return gamenumber
+}
+
+// Returns Information About The Round Fed Into The Function:
+// Output Is Generated In This Format | [red, green, blue]
+func ExtractRoundInformation(s string) [3]int {
+	var colorinfo [3]int
+	s = s + string(",  ") // Add , to the end will help us out later
+	var roundinfo []string
+	for {
+		roundinfo = append(roundinfo, s[:strings.Index(s, ",")]) //Information About 1 Cube
+		s = s[strings.Index(s, ",")+2:]                          // Trim Info We Just Took Out Of s
+		if len(s) < 2 {                                          //Break After We've Completed All Handfulls
+			break
+		}
+	}
+	//fmt.Printf("%s\n", roundinfo[0])
+	for i := 0; i < len(roundinfo); i++ {
+		resultred, foundred := strings.CutSuffix(roundinfo[i], " red")
+		resultgreen, foundgreen := strings.CutSuffix(roundinfo[i], " green")
+		resultblue, foundblue := strings.CutSuffix(roundinfo[i], " blue")
+
+		if foundred {
+			colorinfo[0], _ = strconv.Atoi(resultred)
+		} else if foundgreen {
+			colorinfo[1], _ = strconv.Atoi(resultgreen)
+		} else if foundblue {
+			colorinfo[2], _ = strconv.Atoi(resultblue)
+		}
+	}
+	//fmt.Printf("%d|%d|%d", colorinfo[0], colorinfo[1], colorinfo[2])
+	return colorinfo
 }
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Supporting Functions +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
